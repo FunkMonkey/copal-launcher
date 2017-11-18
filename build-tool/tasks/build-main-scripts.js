3ca68cb5c842e125b422aec71c4da9d83e9e4829
AndSelm <and.selm+git@gmail.com>
2017-11-18 19:30:04 +0100

    MAINT: updating babel & eslint (extracted configuration)

--- build-tool/tasks/build-main-scripts.js
+++ build-tool/tasks/build-main-scripts.js
@@ -1,21 +1,25 @@
-var gulp = require( 'gulp' );
-var babel = require( 'gulp-babel' );
+const gulp = require( "gulp" );
+const babel = require( "gulp-babel" );
 
-var onError = require( '../utils' ).onError;
+const onError = require( "../utils" ).onError;
 
-// var sourcemaps = require( 'gulp-sourcemaps' );
+// const sourcemaps = require( "gulp-sourcemaps" );
 
-var SRC_GLOB =  './src/main/**/*.js';
+const SRC_GLOB =  "./src/main/**/*.js";
 
-gulp.task( 'build:main:scripts', function() {
+gulp.task( "build:main:scripts", function() {
   return gulp.src( SRC_GLOB )
-            //  .pipe( sourcemaps.init() )
-             .pipe( babel( {
-                  presets: ['es2015']
-              } ) )
-             .on( 'error', onError )
-            //  .pipe( sourcemaps.write( '.' ) )
-             .pipe( gulp.dest( 'build/main' ) );
+    //  .pipe( sourcemaps.init() )
+    .pipe(
+      babel( {
+        presets: [[
+          '@babel/preset-env',
+          { "targets": { "node": "6.10" } }
+        ]]
+      } ) )
+    .on( "error", onError )
+    //  .pipe( sourcemaps.write( "." ) )
+    .pipe( gulp.dest( "build" ) );
 });
 
-gulp.tasks[ 'build:main:scripts' ].SRC_GLOB = SRC_GLOB;
+gulp.tasks[ "build:main:scripts" ].SRC_GLOB = SRC_GLOB;
--- build-tool/tasks/build-renderer-scripts.js
+++ build-tool/tasks/build-renderer-scripts.js
@@ -1,21 +1,25 @@
-var gulp = require( 'gulp' );
-var babel = require( 'gulp-babel' );
+const gulp = require( 'gulp' );
+const babel = require( 'gulp-babel' );
+const sourcemaps = require( 'gulp-sourcemaps' );
 
-var onError = require( '../utils' ).onError;
+const onError = require( '../utils' ).onError;
 
-var sourcemaps = require( 'gulp-sourcemaps' );
-
-var SRC_GLOB =  './src/renderer/scripts/**/*.js';
+const SRC_GLOB =  './src/renderer/scripts/**/*.js';
 
 gulp.task( 'build:renderer:scripts', function() {
   return gulp.src( SRC_GLOB )
-             .pipe( sourcemaps.init() )
-             .pipe( babel( {
-                  presets: ['react', 'es2015']
-              } ) )
-             .on( 'error', onError )
-             .pipe( sourcemaps.write( '.' ) )
-             .pipe( gulp.dest( 'build/renderer/scripts' ) );
+    .pipe( sourcemaps.init() )
+    .pipe( babel( {
+        presets: [
+          '@babel/react',
+          [ '@babel/preset-env',
+            { "targets": { "node": "6.10" } }
+          ]
+        ]
+    } ) )
+    .on( 'error', onError )
+    .pipe( sourcemaps.write( '.' ) )
+    .pipe( gulp.dest( 'build/renderer/scripts' ) );
 });
 
 gulp.tasks[ 'build:renderer:scripts' ].SRC_GLOB = SRC_GLOB;
