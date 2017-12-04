5d8255c8899e7474c430dce8dda3ca533f32cc66
AndSelm <and.selm+git@gmail.com>
2017-12-04 20:31:20 +0100

    FORMAT: adding TODO for later

--- src/renderer/scripts/entry.js
+++ src/renderer/scripts/entry.js
@@ -5,6 +5,10 @@ import Main from './ui/main';
 
 // eslint-disable-next-line import/prefer-default-export
 export function init() {
+
+  // TODO: extract core from Launcher, turn Launcher into plugin and wait here for
+  // core.init( pluginInfos ).withLatestFrom( core.plugins.waitFor( '@copal/launcher/controller' ))
+
   const launcher = new Launcher();
   // TODO: find out, why error gets eaten here
   launcher.init()
